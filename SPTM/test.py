9133167ead37061c096e3ed711d6ed857aa1ab0e
muriatec <muriatec2133@gmail.com>
2023-02-04 16:28:21 +0800

    first commit

---
+++ SPTM/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+
+path1 = "dataset_pano/control/Beechwood_0_int/rgb_pano/000_0001.npy"
+# path2 = "dataset/control/Beechwood_0_int/rgb/000_0050.npy"
+
+img1 = np.load(path1, allow_pickle=True)
+img1 = img1[:,:,::-1]
+# img2 = np.load(path2, allow_pickle=True)
+# img2 = img2[:,:,::-1]
+
+cv.imshow("p1", img1)
+# cv.imshow("p2", img2)
+cv.waitKey(0)
+cv.destroyAllWindows()
